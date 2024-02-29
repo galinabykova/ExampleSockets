1227c12b6b1678b2a0f23a80fb56328dda27adfe
Galina Bykova <g.bykova@g.nsu.ru>
2024-02-29 12:04:58 +0700

    bad doc

---
+++ src/test/java/ru/nsu/bykova/SleepingTaskTest.java
@@ -0,0 +1,14 @@
+package ru.nsu.bykova;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.nsu.bykova.data.SleepingTask;
+
+public class SleepingTaskTest {
+    @Test
+    void SleepingTimeLessZero() {
+        final int[] array = new int[]{1};
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> {new SleepingTask(-1L, array);});
+    }
+}
